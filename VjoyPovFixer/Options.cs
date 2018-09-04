b246e4f3eff0c66053227f834d2e06f4696f4877
aradtamako <40272766+aradtamako@users.noreply.github.com>
2018-09-04 23:26:54 +0900

    first commit

---
+++ VjoyPovFixer/Options.cs
@@ -0,0 +1,13 @@
+﻿using CommandLine;
+
+namespace VjoyPovFixer
+{
+    public class Options
+    {
+        [Option('i', "index", Required = false, HelpText = "Vjoy device index", Default = 0)]
+        public int VjoyDeviceIndex { get; set; }
+
+        [Option('q', "quit", Required = false, HelpText = "Auto quit", Default = false)]
+        public bool AutoQuit { get; set; }
+    }
+}
---
+++ VjoyPovFixer/Program.cs
@@ -0,0 +1,33 @@
+﻿using CommandLine;
+using System;
+
+namespace VjoyPovFixer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = Parser.Default.ParseArguments<Options>(args);
+
+            if (result.Tag == ParserResultType.Parsed)
+            {
+                var parsed = (Parsed<Options>)result;
+
+                int deviceIndex = parsed.Value.VjoyDeviceIndex;
+
+                var vjoy = new VJoy();
+                vjoy.Initialize();
+                vjoy.Reset();
+                vjoy.SetPOV(deviceIndex, 0, VJoy.POVType.Nil);
+                vjoy.Update(deviceIndex);
+                vjoy.Shutdown();
+
+                Console.WriteLine("successfully finished");
+                if (!parsed.Value.AutoQuit)
+                {
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
